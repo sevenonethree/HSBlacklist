17118bb0bd6dc02372e825940a4e8e7e5960e0c5
Tre Guillory <tre.guillory@zantler.com>
2014-10-02 18:38:28 -0500

    Added Poco for Employee

---
+++ HSBlacklist/Models/Pocos/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HSBlacklist.Models.Pocos
+{
+    public class Employee
+    {
+        string Name { get; set; }
+        string Location { get; set; }
+        string JobTitle { get; set; }
+        string Email { get; set; }
+        string Phone { get; set; }
+    }
+}
