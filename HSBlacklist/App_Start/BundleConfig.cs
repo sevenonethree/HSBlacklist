0432660a14918e4f32468168c6e99ea356b54dcb
Tre Guillory <tre.guillory@zantler.com>
2014-10-02 18:09:33 -0500

    Added AngularJS and related dependencies. Need to clean up the project to remove unnecessary files

--- HSBlacklist/App_Start/BundleConfig.cs
+++ HSBlacklist/App_Start/BundleConfig.cs
@@ -24,6 +24,8 @@ namespace HSBlacklist
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js"));
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
